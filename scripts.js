a899c4ef5fde6d4af754f53926a91dbdafad71bc
Your Name <you@example.com>
2016-09-25 16:36:58 -0700

    create links to full article with headlines

--- scripts.js
+++ scripts.js
@@ -1,31 +1,36 @@
 // Set url var to NYT article search API, set query parameter to "beer"//
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 
+var currentPage = 0;
+
 url += '?' + $.param({
         'api-key': "2dcedd35edf7491e8d77d447157038de",
         'sort': "newest"
     });
 
-function loadPage(page,q,fq){
+//create a function takes in arguments to create url for API request//
+function loadPage(page, q, fq) {
     $.ajax({
-    method: 'GET',
-    url: url + "?page="+ page + "?q=" + q + "?fq=headline:('" + fq + "')",
-    dataType: 'json',
-    success: function (data) {
-        console.log(data);
-        var html= "";
-        var results = data.response.docs;
-        console.log(results);
-        //iterate through results array//
-        for (i = 0; i < results.length; i++) {
-            html+= "<li id='results-item'><h2 id='headline'>" + data.response.docs[i].headline.main +"</h2><div id='articleSnippet'>" + data.response.docs[i].snippet +"</div></li>";
+        method: 'GET',
+        url: url + "&page=" + page + "&q=" + q + "&fq=headline:('" + fq + "')",
+        dataType: 'json',
+        success: function (data) {
+            console.log(data);
+            var html = "";
+            var results = data.response.docs;
+            console.log(results);
+            //iterate through results array and append to HTML//
+            for (i = 0; i < results.length; i++) {
+                html += "<li id='results-item'><h2 id='headline'><a href='" + data.response.docs[i].web_url +"'>" + data.response.docs[i].headline.main + "</a></h2><p id='articleSnippet'>" + data.response.docs[i].snippet + "</p></li>";
+            }
+            $("#results-list").html(html);
+            currentPage = page;
+            console.log(currentPage);
         }
-     $("#results-list").html(html)
-    }
 
-});
+    });
 }
-loadPage(0,"beer", "beer");
+loadPage(currentPage, "beer", "beer");
 
 
 
