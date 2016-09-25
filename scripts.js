d781417f568eab2de42841d0caa102e2d16f6365
Your Name <you@example.com>
2016-09-25 11:53:06 -0700

    init commit

---
+++ scripts.js
@@ -0,0 +1,24 @@
+// Set url var to NYT article search API, set query parameter to "beer"//
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+        'api-key': "2dcedd35edf7491e8d77d447157038de",
+        'q': "beer",
+        'fq': "headline:('beer')",
+        'sort': "newest",
+        'page': "0"
+    });
+//
+$.ajax({
+    method: 'GET',
+    url: url,
+    dataType: 'json',
+    success: function (data) {
+        console.log(data);
+        var results = data.response.docs; //create an array from json results//
+        console.log(results);
+        for (i = 0; i < results.length; i++) {
+            $("#resultsList").append((data.response.docs[i].headline.main) + "<br>");//display headline for each result in HTML//
+            $("#resultsList").append((data.response.docs[i].snippet) + "<br>"); //display each result in HTML//
+        }
+    }
+});
