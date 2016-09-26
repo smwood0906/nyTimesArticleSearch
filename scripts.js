d43c23d64719496ba9874f5ee198b908c03c5553
Your Name <smarntson@gmail.com>
2016-09-25 20:40:21 -0700

    add new keyword search and button functionality, refactor HTML display code

--- scripts.js
+++ scripts.js
@@ -12,16 +12,16 @@ url += '?' + $.param({
         'sort': "newest"
     });
 
-// create
-// a
-// function that
-// passes in global
-// variable as arguments
-// to
-// create
-// url
-// for API request//
+// create a function that passes in global variable as arguments to create url for API request//
 function loadPage(page, q, fq) {
+    //Do not display back button if first page of results list is being displayed//
+    if (currentPage == 0) {
+        $("#back").css("display", "none");
+    }
+    else {
+        $("#back").css("display", "inline");
+    }
+    //ajax request to NYT article search API//
     $.ajax({
         method: 'GET',
         url: url + "&page=" + page + "&q=" + q + "&fq=headline:('" + fq + "')",
@@ -31,6 +31,13 @@ function loadPage(page, q, fq) {
             var html = "";
             var results = data.response.docs;
             console.log(results);
+            //display current keyword search in HTML//
+            keyword = q;
+            $("#current-search").html("Keyword: " + keyword);
+            $("#keyword").html(keyword);
+            //display total number of results in HTML//
+            total = data.response.meta.hits;
+            $("#total-results").html("Search Results: " + total);
             //iterate through results array and append selected information to HTML//
             for (i = 0; i < results.length; i++) {
                 html += "<li id='results-item'><h2 id='headline-container'><a class='headline' href='" + data.response.docs[i].web_url + "'>" + data.response.docs[i].headline.main + "</a></h2><p id='articleSnippet'>" + data.response.docs[i].snippet + "</p></li>";
@@ -45,13 +52,27 @@ function loadPage(page, q, fq) {
 //run function to load page with data from NYT article search API//
 loadPage(currentPage, q, fq);
 
-// //create a click event that allows user to display next 10 search results//
-    $("#forward").click(function () {
-        event.preventDefault();
-        currentPage += 1;
-        
-    });
+//create a click event that allows user to display next 10 search results//
 
+$("#forward").click(function () {
+    event.preventDefault();
+    currentPage += 1;
+    loadPage(currentPage, q, fq)
+});
+
+$("#back").click(function () {
+    event.preventDefault();
+    currentPage -= 1;
+    loadPage(currentPage, q, fq)
+});
+
+//create a click event that allows users to enter a new keyword and return new search results//
+$("#sub").click(function () {
+    event.preventDefault();
+    q = $("#search-key").val();
+    fq = $("#search-key").val();
+    loadPage(currentPage, q, fq)
+});
 
 
 
